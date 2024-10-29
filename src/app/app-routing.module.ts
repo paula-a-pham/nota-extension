86fe6e97a0d62fe7fe19be0c9ca10d834905ce16
Paula Ashraf <paula.a.pham@gmail.com>
2024-10-29 18:13:30 +0300

    setup create note component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CreateNoteComponent } from './components/create-note/create-note.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'create-note', component: CreateNoteComponent },
 ];
 
 @NgModule({
